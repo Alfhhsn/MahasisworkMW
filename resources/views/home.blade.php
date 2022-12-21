8d12fa41faae8e0490b896fa9e69231291b4aa31
Alfhhsn <aisyahusnalifah30@gmail.com>
2022-12-21 11:21:29 +0700

    push pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,205 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/home.css">
+  </head>
+  <body>
+    
+    <header>
+      <nav class="navbar fixed-top navbar-expand-lg bg-transparent">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Mahasis<span class="pink">Work</span></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle categories" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Categories
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Mentor Course</a></li>
+                  <li><a class="dropdown-item" href="#">Graphic Design</a></li>
+                  <li><a class="dropdown-item" href="#">Translation</a></li>
+                  <li><a class="dropdown-item" href="#">Writing</a></li>
+                  <li><a class="dropdown-item" href="#">Photo Editing</a></li>
+                  <li><a class="dropdown-item" href="#">Video Editing</a></li>
+                  <li><a class="dropdown-item" href="#">Programming & Tech</a></li>
+                  <li><a class="dropdown-item" href="#">Data</a></li>
+                </ul>
+              </li>
+              <li class="nav-item profile">
+                <a class="nav-link" href="#">Profile</a>
+              </li>
+            </ul>
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" type="search" placeholder="Ingin cari jasa apa hari ini?" aria-label="Search" style="width: 400px;">
+              <button class="btn btn-primary" style="background-color: #022E57;" type="submit"><img src="img/search.svg" alt=""></button>
+            </form>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <section class="dashboard">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col left">
+            <div class="card user">
+              <div class="card-body card-comp">
+                <h5 class="card-title">Halo User!</h5>
+                <a href="#" class="card-link">Mulai jasa sekarang!</a>
+              </div>
+            </div>
+            <div class="col right">
+              <div class="card">
+                <div class="card-body">
+                  <h5 class="card-title">Ratusan jasa ditawarkan dengan harga yang beragam!</h5>
+                  <a href="#" class="btn btn-primary jasa-btn">Cari Jasa Sekarang!</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="rekomendasi">
+      <div class="container-fluid">
+        <h1 class="title">Rekomendasi untuk kamu</h1>
+        <div class="row">
+          <div class="col-4">
+            <div class="card text-bg-transparent bg-transparent">
+              <img src="img/winter.png" class="card-img" alt="">
+              <div class="card-img-overlay name">
+                <h5 class="card-title"><a href="">Winter</a></h5>
+              </div>
+              <div class="classification">
+                <h5 class="job-categories">Programmer</h5>
+                <p class="rate"><img src="img/star.svg" alt="">5 <span class="ulasan">(15 ulasan)</span></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card text-bg-transparent bg-transparent">
+              <img src="img/karina.png" class="card-img" alt="">
+              <div class="card-img-overlay name">
+                <h5 class="card-title"><a href="">Karina</a></h5>
+              </div>
+              <div class="classification">
+                <h5 class="job-categories">Course Mentor</h5>
+                <p class="rate"><img src="img/star.svg" alt="">5 <span class="ulasan">(15 ulasan)</span></p>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card text-bg-transparent bg-transparent">
+              <img src="img/ningning.png" class="card-img" alt="">
+              <div class="card-img-overlay name">
+                <h5 class="card-title"><a href="">Ningning</a></h5>
+              </div>
+              <div class="classification">
+                <h5 class="job-categories">Design Graphic</h5>
+                <p class="rate"><img src="img/star.svg" alt="">5 <span class="ulasan">(15 ulasan)</span></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="best">
+      <div class="container-fluid">
+        <h1 class="title">Teratas dalam Design Graphic</h1>
+        <div class="row">
+          <div class="col-4">
+            <div class="card text-bg-transparent bg-transparent">
+              <img src="img/ningning.png" class="card-img" alt="">
+              <div class="card-img-overlay name">
+                <h5 class="card-title"><a href="">Ningning</a></h5>
+              </div>
+              <div class="classification">
+                <p class="rate"><img src="img/star.svg" alt="">5 <span class="ulasan">(15 ulasan)</span></p>
+                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos perferendis cupiditate culpa provident iste reiciendis obcaecati repellat? Assumenda, magni ab!</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card text-bg-transparent bg-transparent">
+              <img src="img/yangyang.png" class="card-img" alt="">
+              <div class="card-img-overlay name">
+                <h5 class="card-title"><a href="">Yangyang</a></h5>
+              </div>
+              <div class="classification">
+                <p class="rate"><img src="img/star.svg" alt="">4.6 <span class="ulasan">(10 ulasan)</span></p>
+                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos perferendis cupiditate culpa provident iste reiciendis obcaecati repellat? Assumenda, magni ab!</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card text-bg-transparent bg-transparent">
+              <img src="img/mark.png" class="card-img" alt="">
+              <div class="card-img-overlay name">
+                <h5 class="card-title"><a href="">Mark</a></h5>
+              </div>
+              <div class="classification">
+                <p class="rate"><img src="img/star.svg" alt="">4 <span class="ulasan">(10 ulasan)</span></p>
+                <p class="desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos perferendis cupiditate culpa provident iste reiciendis obcaecati repellat? Assumenda, magni ab!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <div class="container-fluid">
+        <div class="row upper">
+          <div class="col">
+            <h1 class="title">MahasisWork</h1> <br>
+            <img src="img/instagram.svg" alt="">
+            <img src="img/twitter.svg" alt="">
+          </div>
+          <div class="col">
+            <h1 class="title">Categories</h1>
+            <ul>
+              <li><a href="">Mentor Course</a></li>
+              <li><a href="">Graphic Design</a></li>
+              <li><a href="">Translation</a></li>
+              <li><a href="">Writing</a></li>
+              <li><a href="">Photo Editing</a></li>
+              <li><a href="">Video Editing</a></li>
+              <li><a href="">Programming & Tech</a></li>
+              <li><a href="">Data</a></li>
+            </ul>
+          </div>
+          <div class="col">
+            <h1 class="title">Community</h1>
+            <ul>
+              <li><a href="">Blog</a></li>
+              <li><a href="">Events</a></li>
+              <li><a href="">Forums</a></li>
+              <li><a href="">Invite Friends</a></li>
+            </ul>
+          </div>
+          <div class="col">
+            <h1 class="title">Support</h1>
+            <ul>
+              <li><a href="">Help & Support</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row lower">
+          <h3 class="title">Copyright <span class="bold">MahasisWork</span> 2022</h3>
+        </div>
+      </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ resources/views/landing.blade.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/landing.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700;900&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <title>MahasisWork</title>
+</head>
+<body>
+    <header>
+    <!-- navigation -->
+      <section class="navigation">
+        <div class="container">
+            <div class="box-navigation">
+                <div class="box">
+                    <h1>MahasisWork</h1>
+                </div>
+                <div class="box menu-navigation">
+                    <ul>
+                        <li>
+                            <i class="ri-home-4-line"></i>
+                            <a href="http://mahasisworkmw.test/home">Home</a>
+                        </li>
+                        <li>
+                            <i class="ri-question-line"></i>
+                            <a href="http://mahasisworkmw.test/search">Become a freelance</a>
+                        </li>
+                        <li>
+                            <i class="ri-login-box-line"></i>
+                            <a href="http://mahasisworkmw.test/login">Login</a>
+                        </li>
+                        <li>
+                            <i class="ri-login-circle-line"></i>
+                            <a href="http://mahasisworkmw.test/sign-up">Sign-up</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="box menu-bar">
+                    <i class="ri-menu-3-fill" style="color: white;"></i>
+                </div>
+            </div>
+        </div>
+      </section>  
+    <!-- navigation -->
+
+    <!-- hero -->
+    <section class="hero">
+        <div class="container">
+            <div class="box-hero">
+                <div class="box">
+                <h1>Starting freelance as easy as make a Social Media</h1>
+                </div>
+                <div class="box">
+                <img src="img/hero1.svg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- hero -->
+</header>
+
+<!-- why choose -->
+<section class="choose">
+    <div class="container">
+        <div class="box-choose">
+            <div class="box">
+                <h1>Why choose MahasisWork?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium nam recusandae, facilis iste distinctio optio assumenda cumque, deleniti amet ipsam alias reprehenderit. Praesentium ratione necessitatibus magni nobis fuga, dicta incidunt.</p>
+            </div>
+            <div class="box">
+                <img src="img/Why choose.png" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- why choose -->
+
+<!-- Popular Service -->
+<section class="popular">
+    <div class="container">
+        <div class="box-popular">
+            <div class="box">
+                <img src="img/Mentor.png" alt=""/>
+                <h1>Course Mentor</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque accusamus autem vero. Sapiente, provident officia?</p>
+            </div>
+            <div class="box">
+                <img src="img/Design.png" alt=""/>
+                <h1>Design</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque accusamus autem vero. Sapiente, provident officia?</p>
+            </div>
+            <div class="box">
+                <img src="img/translation.png" alt=""/>
+                <h1>Translator</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque accusamus autem vero. Sapiente, provident officia?</p>
+            </div>
+            <div class="box">
+                <img src="img/writing.png" alt=""/>
+                <h1>Course Mentor</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque accusamus autem vero. Sapiente, provident officia?</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Popular Service -->
+
+<!-- Our Commitment -->
+<section class="commitment">
+    <div class="container">
+        <div class="box-commitment">
+            <div class="box">
+                <img src="img/our commit.png" alt="">
+            </div>
+            <div class="box">
+                <h1>Our Commitment?</h1>
+                <h3 class="desc">“We make <span class="pink">MahasisWork</span>  as a place for student to <span class="pink">start their own careers</span>, and <span class="pink">develop their skills</span>, so that they can achieve their dreams careers”</h3>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Our Commitment -->
+
+<!-- Footer -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <h4>Categories</h4>
+                <ul>
+                <li><a href="#">Mentor Course</a></li>
+                    <li><a href="#">Graphic Design</a></li>
+                    <li><a href="#">Translation</a></li>
+                    <li><a href="#">Writing</a></li>
+                    <li><a href="#">Photo Editing</a></li>
+                    <li><a href="#">Video Editing</a></li>
+                    <li><a href="#">Programming & Tech</a></li>
+                    <li><a href="#">Data</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Community</h4>
+                <ul>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Event</a></li>
+                    <li><a href="#">Forums</a></li>
+                    <li><a href="#">Invite a friends</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Support</h4>
+                <ul>
+                    <li><a href="#">Help & Support</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Follow us</h4>
+                <div class="social-links">
+                    <a href="https://www.instagram.com/hsnasyah/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.twitter.com/nuguhayow/"><i class="fab fa-twitter"></i></a>
+                </div>
+            </div>
+            <div class="row lower">
+                <span class="copyright">Copyright <a href="">MahasisWork</a> 2022</span>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Footer -->
+
+<script src="js/landing.js"></script>
+</body>
+</html>
